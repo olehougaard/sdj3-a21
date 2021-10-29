70117ab2686850622e05e30b5247ceca8593d659
Ole Hougaard <olehougaard@gmail.com>
2021-10-29 13:49:33 +0200

    Fixed erroneous endpoint

--- webbank/bank_branch/src/main/java/dk/via/bank/RunBranch.java
+++ webbank/bank_branch/src/main/java/dk/via/bank/RunBranch.java
@@ -5,7 +5,7 @@ import java.net.URL;
 
 public class RunBranch {
 	public static void main(String[] args) throws Exception {
-		RemoteBranch branch = new RemoteBranch(1234, new URL("http://localhost:8080/bank_hq"));
+		RemoteBranch branch = new RemoteBranch(1234, new URL("http://localhost:8080/"));
 		Endpoint.publish("http://localhost:8090/branch", branch);
 	}
 }
